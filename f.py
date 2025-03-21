c25fcd8c042626a506bc16706c5559425c35f731
vstlvl <zorix228337@gmail.com>
2025-03-21 21:56:28 +0300

    написал класс книги и тест-процедуру к ней

--- f.py
+++ f.py
@@ -1,4 +1,28 @@
 # Книга
+class Book:
+    title: str
+    author: str
+    genre: str
+    year: int
+    pages: int
+    
+    def __init__(self, title, author, genre, year, pages):
+        self.author = author
+        self.genre = genre
+        self.pages = pages
+        self.title = title
+        self.year = year
+    
+    def info(self) -> None:
+        print(f"  {self.title}\n  {self.author}\n  genre: {self.genre}\n  {self.year}\n  pages - {self.pages}\n")
+        
+    def is_long(self) -> bool:
+        return (self.pages > 300)
+
+def book_test() -> None:
+    book = Book("test", "author", "312", 42, 54)
+    book.info()
+    book.is_long()
 
 # Студент
 
