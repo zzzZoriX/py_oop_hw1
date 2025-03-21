86cadc903a339fcf23d05e984c16700c83966829
vstlvl <zorix228337@gmail.com>
2025-03-21 22:03:11 +0300

    написал класс студента и тест-процедуру к нему

--- f.py
+++ f.py
@@ -17,7 +17,7 @@ class Book:
         print(f"  {self.title}\n  {self.author}\n  genre: {self.genre}\n  {self.year}\n  pages - {self.pages}\n")
         
     def is_long(self) -> bool:
-        return (self.pages > 300)
+        return self.pages > 300
 
 def book_test() -> None:
     book = Book("test", "author", "312", 42, 54)
@@ -25,6 +25,30 @@ def book_test() -> None:
     book.is_long()
 
 # Студент
+class Student:
+    name: str
+    university: str
+    age: int
+    year_of_study: int
+    gpa: int
+    
+    def __init__(self, name, age, university, year_of_study, gpa):
+        self.age = age
+        self.gpa = gpa
+        self.name = name
+        self.university = university
+        self.year_of_study = year_of_study
+        
+    def study_year(self) -> None:
+        print(f"{self.year_of_study}")
+        
+    def is_excellent(self) -> bool:
+        return self.gpa >= 4.5
+    
+def student_test() -> None:
+    student = Student("student", 123, "any", 321, 4.5)
+    student.is_excellent()
+    student.study_year()
 
 # Авто
 
